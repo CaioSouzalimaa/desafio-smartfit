5ab58b9639869578084752862b14d43a4741713b
Caio Souza Lima <caiosouzalima2002@gmail.com>
2023-10-20 21:03:01 -0300

    feat: add footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { CardsListComponent } from './components/cards-list/cards-list.component
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './components/card/card.component';
 import { LegendComponent } from './components/legend/legend.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
@@ -21,6 +22,7 @@ import { LegendComponent } from './components/legend/legend.component';
     CardsListComponent,
     CardComponent,
     LegendComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
